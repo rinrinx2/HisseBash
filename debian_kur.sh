58915881c555d74e2c287ee8570b7ce1ccb2940e
Ömer Faruk Sancak <keyiflerolsun@gmail.com>
2021-03-07 04:48:04 +0300

    Update debian_kur.sh

--- debian_kur.sh
+++ debian_kur.sh
@@ -44,11 +44,13 @@ sudo gem install colorls
 sudo apt autoremove -y
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt update -y
-sudo apt install python3.9
+sudo apt install python3.9 -y
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 2
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.9 2
+pip install thefuck
+pip install -U psutil
 
 # keyiflerolsun
 rm -rf ~/.zshrc && wget https://raw.githubusercontent.com/HissePlus/HisseBash/main/.zshrc
